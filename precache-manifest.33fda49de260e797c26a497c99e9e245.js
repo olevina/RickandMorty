ece6ad047d748bc23eee8ec3837cb6e7675be453
Olevina <olevina@ymail.com>
2018-12-29 19:54:48 -0700

    Updates

---
+++ precache-manifest.33fda49de260e797c26a497c99e9e245.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d545c939dd1b4c7b6be5",
+    "url": "/RickandMorty/static/js/runtime~main.d545c939.js"
+  },
+  {
+    "revision": "3a9d33e39fd1b0642387",
+    "url": "/RickandMorty/static/js/main.3a9d33e3.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/RickandMorty/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "3a18ab2c3b88900ab42a9367e24f1ff7",
+    "url": "/RickandMorty/index.html"
+  }
+];
